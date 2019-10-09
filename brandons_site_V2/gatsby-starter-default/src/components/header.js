028ec298bc802a118749d9dcff28d66116786fab
John Oncher <johnoncher@gmail.com>
2019-10-09 14:24:23 -0400

    menu and drawer added

---
+++ brandons_site_V2/gatsby-starter-default/src/components/header.js
@@ -0,0 +1,104 @@
+import PropTypes from "prop-types"
+import React, { useState } from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import Icon from "@material-ui/core/Icon"
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer"
+import Button from "@material-ui/core/Button"
+import List from "@material-ui/core/List"
+import Divider from "@material-ui/core/Divider"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemIcon from "@material-ui/core/ListItemIcon"
+import ListItemText from "@material-ui/core/ListItemText"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+}))
+
+function Header(siteTitle) {
+  const classes = useStyles()
+
+  const [state, setState] = useState({ left: false })
+
+  const toggleDrawer = (side, open) => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return
+    }
+
+    setState({ ...state, [side]: open })
+  }
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <Icon>inbox</Icon> : <Icon>mail</Icon>}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <Icon>inbox</Icon> : <Icon>mail</Icon>}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  )
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Icon
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer("left", true)}
+          >
+            menu
+          </Icon>
+          <Typography variant="h6" color="inherit">
+            Brandon's Site
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <SwipeableDrawer open={state.left} onClose={toggleDrawer("left", false)}>
+        {sideList("left")}
+      </SwipeableDrawer>
+    </div>
+  )
+}
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
